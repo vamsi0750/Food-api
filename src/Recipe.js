0779f509580c40c1c82eb9500e9d3fbcd98e6c06
vamsi0750 <nagisettyvamsi143@gmail.com>
2019-08-04 11:13:00 +0530

    adding title

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Recipe({title,calories,image,ingredientLines}) {
  return (
   <div className="boxx">
+    <h1>vamsi Rest</h1>
    <h1>{title}</h1>
    <p><b>calories: </b>{calories}</p>
    <img src={image} alt="img here" />
